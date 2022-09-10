f33b810067032c2f87ad09e250c2d6637cb45cc5
Rishav Raj <iamrishavraj1@gmail.com>
2022-09-10 22:31:08 +0530

    Add Errors and Middleware

--- app.js
+++ app.js
@@ -3,9 +3,21 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT | 3000;
 const jobsRouter = require("./routes/jobsRoutes");
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
 
 app.use("/api/v1/jobs", jobsRouter);
 
-app.listen(port, () => console.log("server listening"));
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const start = async () => {
+  try {
+    app.listen(port, () => console.log(`Server is listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
